f568238c40c324e708097dc70dfcd25bf2274a3c
JaQuan Banks <shadic151@gmail.com>
2022-09-20 01:15:51 -0400

    commit tree ff50ef581328ff198fe6800e33dc523300c0ff9d

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+const employee = {name: "Sam", streetAddress: "11 Broadway"}
+
+function updateEmployeeWithKeyAndValue() {
+   var extraNewEmployee = {...employee};
+   extraNewEmployee = {name: "Sam", streetAddress: "11 Broadway"}
+   const employee = {...employee, ...extraNewEmployee};
+   return employee
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue() {
+    employee.streetAddress = "12 Broadway"
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, name) {
+    const newEmployee = {...employee}
+    delete newEmployee.name
+    return newEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, name) {
+    delete employee.name
+    return employee
+}
